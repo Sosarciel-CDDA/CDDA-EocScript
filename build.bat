0e73362a6be5af1ee8e6420675e62185926d8541
zwa73 <ssfen2419@126.com>
2023-05-05 21:50:32 +0800

    fix build error

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-rem pkg app.js --targets node14-win-x64 --output ./bin/Release/node14-win-x64/EocScript.exe
 xcopy /E /I /Y "./dist" "./bin/Release/node14-win-x64/dist"
+pkg app.js --targets node14-win-x64 --output ./bin/Release/node14-win-x64/EocScript.exe
 pause
