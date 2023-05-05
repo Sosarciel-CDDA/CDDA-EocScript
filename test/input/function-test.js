931d25f83ddbd23adf0c84c3386e2c1193cf6f8f
zwa73 <ssfen2419@126.com>
2023-05-05 12:10:30 +0800

    test json

---
+++ test/input/function-test.js
@@ -0,0 +1,7 @@
+function regenVal(id){
+  u_val(id)+=1;
+  return 123;
+}
+regenVal(a)
+regenVal(b)
+let c = regenVal(b)+1;
---
+++ test/input/test.js
@@ -0,0 +1,19 @@
+EOC_TYPE("test")
+deactivate_condition(a==1)
+recurrence([1,60])
+function regenVatmin(维他命){
+	u_val(维他命)+=1;
+}
+
+i+=1;
+if(i>=3){
+	i=0;
+	regenVatmin('维他命A')
+	regenVatmin('维他命B')
+	regenVatmin('维他命C')
+	regenVatmin('维他命D')
+}
+earr([
+	{earrt:1},
+	{earrt:2},
+])
